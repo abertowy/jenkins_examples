9bfdebe8798f891f2c6f1882687cb0d127338a63
Daria ODintsova <abertowy@gmail.com>
2026-02-12 17:20:21 +0100

    Mixed approach

--- test_post.groovy
+++ test_post.groovy
@@ -2,31 +2,33 @@ library('purpdm-lib@main')
 
 pipeline {
     agent any
-    // stageBuild(pipelineParams)
+    
     stages {
-        stage('Initialize') {
-            steps {
-                script {
-                    echo "BUILD"
-                }
-            }
-        }
+        stageBuild(pipelineParams)
+        // stage('Initialize') {
+        //     steps {
+        //         script {
+        //             echo "BUILD"
+        //         }
+        //     }
+        // }
     }
-    post{
-        success {
-            script {
-                echo "SUCCESS"
-            }
-		}        
-        failure {
-            script {
-                echo "TFAILURE"
-            }
-        }
-        always{
-            script {
-                echo "ALWAYS"
-            }
-        }
-    }
+    stageTest(pipelineParams)
+    // post{
+    //     success {
+    //         script {
+    //             echo "SUCCESS"
+    //         }
+	// 	}        
+    //     failure {
+    //         script {
+    //             echo "TFAILURE"
+    //         }
+    //     }
+    //     always{
+    //         script {
+    //             echo "ALWAYS"
+    //         }
+    //     }
+    // }
 }
