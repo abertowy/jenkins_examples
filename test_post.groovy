08bbcbf023bc59f3925096be736d8ad88255f02d
Daria ODintsova <abertowy@gmail.com>
2026-02-12 17:16:17 +0100

    Create regular post job

---
+++ test_post.groovy
@@ -0,0 +1,32 @@
+@Library('purpdm-lib@main')
+
+pipeline {
+    agent any
+    // stageBuild(pipelineParams)
+    stages {
+        stage('Initialize') {
+            steps {
+                script {
+                    echo "BUILD"
+                }
+            }
+        }
+    }
+    post{
+        success {
+            script {
+                echo "SUCCESS"
+            }
+		}        
+        failure {
+            script {
+                echo "TFAILURE"
+            }
+        }
+        always{
+            script {
+                echo "ALWAYS"
+            }
+        }
+    }
+}
