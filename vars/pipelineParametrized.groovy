be0279f2dc89e80a303894b2507a7851fd8f852d
Daria ODintsova <abertowy@gmail.com>
2026-02-12 17:03:07 +0100

    Try without try-catch block

--- vars/pipelineParametrized.groovy
+++ vars/pipelineParametrized.groovy
@@ -1,16 +1,18 @@
 def call(Map pipelineParams) {
     pipeline {
     // agent any
-        try {
-            stageBuild(pipelineParams)
-        } catch (Throwable ex) {
-            throw ex
-        } finally {
-            if (currentBuild.result == 'SUCCESS') {
-            }
-            if (currentBuild.result == 'UNSTABLE') {
-            }
-            stageTest(pipelineParams)
-        }
+        stageBuild(pipelineParams)
+        stageTest(pipelineParams)
+        // try {
+        //     stageBuild(pipelineParams)
+        // } catch (Throwable ex) {
+        //     throw ex
+        // } finally {
+        //     if (currentBuild.result == 'SUCCESS') {
+        //     }
+        //     if (currentBuild.result == 'UNSTABLE') {
+        //     }
+        //     stageTest(pipelineParams)
+        // }
     }
 }
