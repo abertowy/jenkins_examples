ba739b08fdad3d53f7fb08257a39d7ee7a28b8f3
Daria ODintsova <abertowy@gmail.com>
2026-02-12 18:15:27 +0100

    Dummy #5

--- vars/stageTest.groovy
+++ vars/stageTest.groovy
@@ -1,27 +1,27 @@
 package vars
 
 def call(Map pipelineParams) {
-    // post{
-    //     success {
-    //         script {
-    //             echo "SUCCESS"
-    //         }
-	// 	}        
-	// 	failure {
-    //         script {
-    //             echo "TFAILURE"
-    //         }
-    //     }
-	// 	always{
-    //         script {
-    //             echo "ALWAYS"
-    //         }
-    //     }
-    // }
-    // 
-    stage('TEST'){
-        script{
-            echo "TEST SOME CODE"
+    post{
+        success {
+            script {
+                echo "SUCCESS"
+            }
+		}        
+		failure {
+            script {
+                echo "TFAILURE"
+            }
+        }
+		always{
+            script {
+                echo "ALWAYS"
+            }
         }
     }
+    
+    // stage('TEST'){
+    //     script{
+    //         echo "TEST SOME CODE"
+    //     }
+    // }
 }
