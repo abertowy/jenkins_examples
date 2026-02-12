86f8a598f3964cee5d729e46e85294204c3155e0
Daria ODintsova <abertowy@gmail.com>
2026-02-12 17:24:51 +0100

    Test mixed app#3

--- test_post.groovy
+++ test_post.groovy
@@ -3,32 +3,31 @@ library('purpdm-lib@main')
 pipeline {
     agent any
     
-    // stages {
-        // stage('Initialize') {
-        //     steps {
-        //         script {
-        //             echo "BUILD"
-        //         }
+    stages {
+        stage('Initialize') {
+            steps {
+                script {
+                    echo "BUILD"
+                }
+            }
+        }
+    }
+    post{
+        stageTest(branch: params.GIT_BRANCH)
+        // success {
+        //     script {
+        //         echo "SUCCESS"
+        //     }
+		// }        
+        // failure {
+        //     script {
+        //         echo "TFAILURE"
         //     }
         // }
-    // }
-    stageBuild(pipelineParams)
-    stageTest(pipelineParams)
-    // post{
-    //     success {
-    //         script {
-    //             echo "SUCCESS"
-    //         }
-	// 	}        
-    //     failure {
-    //         script {
-    //             echo "TFAILURE"
-    //         }
-    //     }
-    //     always{
-    //         script {
-    //             echo "ALWAYS"
-    //         }
-    //     }
-    // }
+        // always{
+        //     script {
+        //         echo "ALWAYS"
+        //     }
+        // }
+    }
 }
