5b07173b28f959897b47e65d2693edd3210bf98a
Daria ODintsova <abertowy@gmail.com>
2026-02-12 17:36:32 +0100

    Dummy #2

--- vars/pipelineParametrized.groovy
+++ vars/pipelineParametrized.groovy
@@ -1,34 +1,7 @@
 def call(Map pipelineParams) {
     pipeline {
     // agent any
-        //stageBuild(pipelineParams)
-        stages {
-            stage('Initialize') {
-                steps {
-                    script {
-                        echo "BUILD"
-                    }
-                }
-            }
-        }
-        post{
-            success {
-                script {
-                    echo "SUCCESS"
-                }
-		    }        
-            failure {
-                script {
-                    echo "TFAILURE"
-                }
-            }
-            always{
-                script {
-                    echo "ALWAYS"
-                }
-            }
-        }
-        // stageTest(pipelineParams)
+        stageTest(pipelineParams)
         // try {
         //     stageBuild(pipelineParams)
         // } catch (Throwable ex) {
