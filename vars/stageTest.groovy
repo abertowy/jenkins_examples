4bc869ede2f8311bab9c5fd9096986c8a4a8e7d6
Daria ODintsova <abertowy@gmail.com>
2026-02-12 17:30:23 +0100

    parametrized again

--- vars/stageTest.groovy
+++ vars/stageTest.groovy
@@ -1,21 +1,24 @@
 package vars
 
 def call(Map pipelineParams) {
-    post{
-        success {
-            script {
-                echo "SUCCESS"
-            }
-		}        
-		failure {
-            script {
-                echo "TFAILURE"
-            }
-        }
-		always{
-            script {
-                echo "ALWAYS"
-            }
-        }
+    // post{
+    //     success {
+    //         script {
+    //             echo "SUCCESS"
+    //         }
+	// 	}        
+	// 	failure {
+    //         script {
+    //             echo "TFAILURE"
+    //         }
+    //     }
+	// 	always{
+    //         script {
+    //             echo "ALWAYS"
+    //         }
+    //     }
+    // }
+    script {
+        echo "ALWAYS"
     }
 }
